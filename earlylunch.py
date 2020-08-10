08b8be972e2aaa61af6cd960de26138cfbcc98c4
Danny Park <danielparkonline@gmail.com>
2020-08-10 16:59:54 -0400

    lab 8 completed

---
+++ earlylunch.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#user_input = input("whats yo name?")
+#print("hello" + user_input)
+#print("let me get an early lunch!")
+
+name_input = input("Whats your name fam?")
+dayofweek_input = input("What day of the week is it?")
+print(f"Hello, {name_input}! Happy {dayofweek_input}!")
+
